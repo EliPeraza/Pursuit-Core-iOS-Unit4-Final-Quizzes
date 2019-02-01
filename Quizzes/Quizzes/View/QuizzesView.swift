a2ee8e13041d9bf535fd6b30a3c0dd0c4670a06c
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-02-01 12:17:17 -0500

    created collection view for Quizzes controller and set constraints

--- Quizzes/Quizzes/View/QuizzesView.swift
+++ Quizzes/Quizzes/View/QuizzesView.swift
@@ -9,13 +9,37 @@
 import UIKit
 
 class QuizzesView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+  
+  lazy var myQuizzesCollectionView: UICollectionView = {
+    
+    let layoutForCollection = UICollectionViewFlowLayout()
+    layoutForCollection.itemSize = CGSize(width: 200, height: 250)
+    layoutForCollection.sectionInset = UIEdgeInsets.init(top: 20, left: 10, bottom: 20, right: 10)
+    
+    let collectionView = UICollectionView.init(frame: self.bounds, collectionViewLayout: layoutForCollection)
+    collectionView.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+    
+    return collectionView
+  }()
+  
+  
+  override init(frame: CGRect) {
+    super.init(frame: UIScreen.main.bounds)
+    addSubview(myQuizzesCollectionView)
+    setCollectionViewConstraints()
+  }
+  
+  //TODO - CHECK THE ALEX THING WITH THE ADDITIONAL INIT - CHECK THE APP WE DID IN CLASS
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  func setCollectionViewConstraints() {
+    myQuizzesCollectionView.translatesAutoresizingMaskIntoConstraints = false
+    myQuizzesCollectionView.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 1).isActive = true
+    myQuizzesCollectionView.widthAnchor.constraint(equalTo: safeAreaLayoutGuide.widthAnchor, multiplier: 1).isActive = true
+  }
+  
+  
 }
